5090593506fd6c942d97574db0ffe998b4c658a6
Energy0004 <Zhandos30101@gmail.com>
2025-05-12 14:19:45 +0500

    Trigger CI/CD

--- final-back/my-frontend/vite.config.js
+++ final-back/my-frontend/vite.config.js
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+   server: {
+    host: '0.0.0.0', // Важно! Это позволяет подключаться снаружи.
+    port: 5173, // Порт, который вы используете
+  },
   plugins: [vue()],
 })
