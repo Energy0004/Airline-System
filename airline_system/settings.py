67479d18959c159b38c909dd663e7adaae32725a
Energy0004 <Zhandos30101@gmail.com>
2025-05-13 23:59:06 +0500

    Trigger CI/CD

--- airline_system/settings.py
+++ airline_system/settings.py
@@ -84,18 +84,29 @@ WSGI_APPLICATION = 'airline_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     # 'default': {
+#         # 'ENGINE': 'django.db.backends.sqlite3',
+#         # 'NAME': BASE_DIR / 'db.sqlite3',
+#     # }
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Maximum0004',
+#         'HOST': 'db',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
-    # 'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'Maximum0004',
-        'HOST': 'db',
-        'PORT': '5432',
+        'NAME': os.getenv('POSTGRES_DB', 'airline_system'),  # Ensure 'airline_system' is used
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'Maximum0004'),
+        'HOST': os.getenv('POSTGRES_HOST', 'db'),  # 'db' is the service name in docker-compose
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
